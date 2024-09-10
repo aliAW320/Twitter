af6b3e0a16c21bee1dd7d3551e25647ad74bbfd2
aliAW320 <alinoroozi13845132@gmail.com>
2024-09-10 21:56:31 +0330

    geting profile added but not tested

--- src/main/java/com/twiter/Twiter12/Controllers/UserController.java
+++ src/main/java/com/twiter/Twiter12/Controllers/UserController.java
@@ -1,8 +1,12 @@
 package com.twiter.Twiter12.Controllers;
 
+import com.twiter.Twiter12.Models.Post;
+import com.twiter.Twiter12.Models.User;
 import com.twiter.Twiter12.Response.Response;
 import com.twiter.Twiter12.Response.ValidationResponse;
+import com.twiter.Twiter12.Response.WholeProfileResponse;
 import com.twiter.Twiter12.Utils.ChangeProfileServices;
+import com.twiter.Twiter12.Utils.Media;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,4 +30,22 @@ public class UserController {
         }
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(code));
     }
+    @GetMapping("/user/allData/{UserName}")
+    @CrossOrigin
+    public ResponseEntity allData(String UserName) {
+        Response response;
+        int code;
+        User user = User.checkNewUser(" ",UserName);
+        if(user== null){
+            code = 400;
+            response = new ValidationResponse(false,"User not found",code);
+            return new ResponseEntity<>(response,HttpStatusCode.valueOf(code));
+        }
+        for (Post post : user.getPosts()) {
+            post.setMedia(Media.uploadMedia(post.getMedia()));
+        }
+        response = new WholeProfileResponse(user.getPosts(),user.getPosts().size(), user.getFollowersCount(), user.getFollowingCount(), user.getUsername(), Media.uploadMedia(user.getProfilePicture()),200);
+        return new ResponseEntity<>(response,HttpStatusCode.valueOf(200)) ;
+    }
+
 }
